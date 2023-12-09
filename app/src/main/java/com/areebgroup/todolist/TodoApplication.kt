74e98a8ddea5b06f49ace246ef41fd7fb3dac1ef
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-12-09 21:39:00 +0200

    create dependency injection module and add annotation on MainActivity

---
+++ app/src/main/java/com/areebgroup/todolist/TodoApplication.kt
@@ -0,0 +1,8 @@
+package com.areebgroup.todolist
+
+import android.app.Application
+import android.content.Context
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TodoApplication: Application()
---
+++ app/src/main/java/com/areebgroup/todolist/di/DatabaseModule.kt
@@ -0,0 +1,33 @@
+package com.areebgroup.todolist.di
+
+import android.content.Context
+import androidx.room.Room
+import com.areebgroup.todolist.data.source.local.dao.TodoDao
+import com.areebgroup.todolist.data.source.local.TodoDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideTodoDao(todoDatabase: TodoDatabase): TodoDao =
+        todoDatabase.todoDao
+
+    @Provides
+    fun provideTodoDatabase(
+        @ApplicationContext context: Context
+    ): TodoDatabase {
+        return Room.databaseBuilder(
+            context,
+            TodoDatabase::class.java,
+            "todo_database"
+        ).fallbackToDestructiveMigration().build()
+    }
+}
