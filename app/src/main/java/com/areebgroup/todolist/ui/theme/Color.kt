44ce8dc391490e9cb3b27e45f6f90ba9e7651ee5
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-12-09 23:06:25 +0200

    add different colors and themes

---
+++ app/src/main/java/com/areebgroup/todolist/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.areebgroup.todolist.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFDACDF7)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val white87 = Color(0xDEFFFFFF)
+val white37 = Color(0x5EFFFFFF)
+
+val black37 = Color(0xFFA0A0A0)
+val black87 = Color(0xDE121212)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF9558A7)
+val Pink40 = Color(0xFF895A96)
